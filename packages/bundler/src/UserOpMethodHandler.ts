f2fd40d05905961ffadd37031a2b4b46849e0f4f
Daniel Tamas <hello@danieltamas.ro>
2025-11-12 17:35:35 +0200

    added log chunking

--- packages/bundler/src/UserOpMethodHandler.ts
+++ packages/bundler/src/UserOpMethodHandler.ts
@@ -168,42 +168,54 @@ export class UserOpMethodHandler {
   }
 
   async _getUserOperationEvent (userOpHash: string): Promise<UserOperationEventEvent> {
-    // TODO: eth_getLogs is throttled. must be acceptable for finding a UserOperation by hash
-    const event = await this.entryPoint.queryFilter(this.entryPoint.filters.UserOperationEvent(userOpHash))
-    return event[0]
+    // Use chunked log fetching to avoid RPC block range limits
+    const provider = this.provider as providers.JsonRpcProvider
+    const currentBlock = await provider.getBlockNumber()
+    
+    // Start from a reasonable range (e.g., last 100k blocks) and expand if needed
+    let fromBlock = Math.max(0, currentBlock - 100_000)
+    
+    const filter = {
+      address: this.entryPoint.address,
+      topics: this.entryPoint.filters.UserOperationEvent(userOpHash).topics
+    }
+    
+    // First try with recent blocks
+    let logs = await getLogsChunked(provider, {
+      ...filter,
+      fromBlock,
+      toBlock: currentBlock
+    }, { maxRange: MAX_RANGE })
+    
+    // If not found in recent blocks, search further back
+    if (logs.length === 0 && fromBlock > 0) {
+      // Search from contract deployment or a reasonable historical point
+      const deploymentBlock = 40321530;
+      logs = await getLogsChunked(provider, {
+        ...filter,
+        fromBlock: deploymentBlock,
+        toBlock: fromBlock - 1
+      }, { maxRange: MAX_RANGE })
+    }
+    
+    if (logs.length === 0) {
+      return null as any
+    }
+    
+    // Parse the log to get the event
+    const parsedLog = this.entryPoint.interface.parseLog(logs[0])
+    const event = {
+      ...logs[0],
+      ...parsedLog,
+      args: parsedLog.args,
+      getTransaction: async () => provider.getTransaction(logs[0].transactionHash),
+      getTransactionReceipt: async () => provider.getTransactionReceipt(logs[0].transactionHash),
+      getBlock: async () => provider.getBlock(logs[0].blockHash)
+    } as any
+    
+    return event
   }
 
-  // filter full bundle logs, and leave only logs for the given userOpHash
-  // @param userOpEvent - the event of our UserOp (known to exist in the logs)
-  // @param logs - full bundle logs. after each group of logs there is a single UserOperationEvent with unique hash.
-  ___filterLogs (userOpEvent: UserOperationEventEvent, logs: Log[]): Log[] {
-    let startIndex = -1
-    let endIndex = -1
-    const events = Object.values(this.entryPoint.interface.events)
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const beforeExecutionTopic = this.entryPoint.interface.getEventTopic(events.find(e => e.name === 'BeforeExecution')!)
-    logs.forEach((log, index) => {
-      if (log?.topics[0] === beforeExecutionTopic) {
-        // all UserOp execution events start after the "BeforeExecution" event.
-        startIndex = endIndex = index
-      } else if (log?.topics[0] === userOpEvent.topics[0]) {
-        // process UserOperationEvent
-        if (log.topics[1] === userOpEvent.topics[1]) {
-          // it's our userOpHash. save as end of logs array
-          endIndex = index
-        } else {
-          // it's a different hash. remember it as beginning index, but only if we didn't find our end index yet.
-          if (endIndex === -1) {
-            startIndex = index
-          }
-        }
-      }
-    })
-    if (endIndex === -1) {
-      throw new Error('fatal: no UserOperationEvent in logs')
-    }
-    return logs.slice(startIndex + 1, endIndex)
-  }
 
   /**
  * Fetch logs chunked from the RPC and return only the logs for the *executed section*
